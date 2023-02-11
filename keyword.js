271df4d4b0910b482b791cd75af67b9cec65f0ea
tannukumari14 <94106847+tannukumari14@users.noreply.github.com>
2023-02-11 21:30:25 +0530

    Add files via upload

---
+++ keyword.js
@@ -0,0 +1,19 @@
+var a=10;
+
+let b=20;
+
+const c=30;
+
+console.log("Values are:",+a+","+b+","+c);
+
+if (true){
+
+    var a=1000;
+
+    let b=2000;
+
+    const c=3000;
+
+    console.log("Values inside the block are:"+a+","+b+","+c)
+
+}
